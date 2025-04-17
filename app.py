f12b7292b71954cbb90c9235f0f4d8b99afcc0b3
DEEVANDO <deevando@gmail.com>
2025-04-17 22:45:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ archivo = st.file_uploader("Sube tu archivo CSV (formato bancario)", type=["csv"
 
 if archivo:
         try:
-        try:
         df = pd.read_csv(archivo, sep=';', encoding='utf-8')
         except UnicodeDecodeError:
         df = pd.read_csv(archivo, sep=';', encoding='ISO-8859-1')
